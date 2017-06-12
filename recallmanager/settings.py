e0ed64d2930fcf2d6125461c5250bf46329bc800
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2017-06-12 16:28:53 +0100

    Added files and changes required for deployment to heroku

--- recallmanager/settings.py
+++ recallmanager/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
